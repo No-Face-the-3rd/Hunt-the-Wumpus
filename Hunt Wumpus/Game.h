f22b7516400a4e7220976917a45a2cb47e7a3ced
Jacob Dodson <foodflare@yahoo.com>
2015-10-20 17:46:11 -0700

    Did work

--- Hunt Wumpus/Game.h
+++ Hunt Wumpus/Game.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "Room.h"
 #include <fstream>
+#include "string.h"
 
 class Game
 {
 private:
-	int state[255], stateRef[255], *layout, latestState, columns, numRooms,numConnect,numWumpus,numBats, numPits,numArrows;
+	int state[255], stateRef[255], *layout, latestState, columns, numRooms,numConnect,numWumpus,numBats, numPits,numArrows,currentState,currentRoom,currentArrows;
 	Room *rooms;
 	std::fstream file;
+	sfw::string stateTypes[10];
+	bool nearWump, nearBats, nearPit;
 public:
 	Game();
 	~Game();
+	void update();
+	void checkRooms();
+	void newGame();
 };
 
